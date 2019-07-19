c41c941e8cb120e358570e4fa02bf5905b5319e8
Suwitpong Kongmee <suwitpongk@gmail.com>
2019-07-19 14:31:30 +0700

    init

---
+++ src/test/java/com/suwitpongk/hello/HelloDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.suwitpongk.hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
